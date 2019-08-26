e7f18b59bea3ad2da9565c98ed3c005f0a8dba8b
Saul Feliz <646634@gmail.com>
2019-08-26 22:19:41 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
 # Your Code Here
+def map(arr)
+result = []
+for i in arr do
+  result << yield(i)
+  end
+return result
+end 
+
+def reduce(arr, starting_point = nil)
+  if starting_point
+  sum = starting_point
+  i = 0
+  else
+  sum = arr[0]
+  i = 1
+  end
+
+  while i < arr.length do
+  sum = yield(sum, arr[i])
+  i += 1
+  end
+return sum
+end
