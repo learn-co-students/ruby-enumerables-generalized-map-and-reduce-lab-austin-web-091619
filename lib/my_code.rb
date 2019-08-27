46168a26aedb592ee1a39236bba87d66d9e0509c
Joelle Helm <joellenhelm@gmail.com>
2019-08-27 20:06:44 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # Your Code Here
+def map(given_array)
+  given_array.map do |below|
+  yield(below)
+  end
+
+end
+
+
+def reduce(source_array, starting_point = 0)
+  start = starting_point
+  if starting_point == 0
+  source_array.reduce { |a, b| yield(a, b)}
+else
+  (source_array.first..source_array.last).each do |n|
+    start += n
+  end
+  return start
+end
+
+end
